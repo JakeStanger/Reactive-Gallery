dec25b9e815a036c090d922a288432f9ef2d84bd
Jake Stanger <mail@jstanger.dev>
2019-11-05 22:42:20 +0000

    use config for contact details

--- src/components/pages/contact/Contact.tsx
+++ src/components/pages/contact/Contact.tsx
@@ -3,31 +3,32 @@ import styles from "./Contact.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faPaypal, faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import config from "../../../config.json";
 
 const Contact: React.FC = () => (
   <div className={styles.container}>
     <div className={styles.title}>Contact</div>
     <div className={styles.links}>
       <div className={styles.link}>
-        <a href="mailto:rstanger.photography@gmail.com">
+        <a href={`mailto:${config.contact.email}`}>
           <FontAwesomeIcon icon={faEnvelope} style={{ width: "unset" }} />
           <div className={styles.label}>Email</div>
         </a>
       </div>
       <div className={styles.link}>
-        <a href="https://www.facebook.com/RogerStangerPhotography" target="_blank" rel="noopener noreferrer">
+        <a href={config.contact.facebook} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faFacebook} style={{ width: "unset" }} />
           <div className={styles.label}>Facebook</div>
         </a>
       </div>
       <div className={styles.link}>
-        <a href="https://www.instagram.com/rogerstangerphotography" target="_blank" rel="noopener noreferrer">
+        <a href={config.contact.instagram} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faInstagram} style={{ width: "unset" }} />
           <div className={styles.label}>Instagram</div>
         </a>
       </div>
       <div className={styles.link}>
-        <a href="https://paypal.me/rstangerphotos" target="_blank" rel="noopener noreferrer">
+        <a href={config.contact.paypal} target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faPaypal} style={{ width: "unset" }} />
           <div className={styles.label}>PayPal</div>
         </a>
