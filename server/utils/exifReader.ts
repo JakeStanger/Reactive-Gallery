71b743f2e6178f56619d973ecc609d91aadaae8e
Jake Stanger <mail@jstanger.dev>
2020-05-25 22:53:01 +0100

    fix: exif dimensions unreliable

--- server/utils/exifReader.ts
+++ server/utils/exifReader.ts
@@ -21,9 +21,6 @@ export async function getExif(image: Buffer) {
   if ((exif as ExifData).exif) {
     const exifRaw = (exif as ExifData).exif;
 
-    imageData.width = exifRaw.ExifImageWidth;
-    imageData.height = exifRaw.ExifImageHeight;
-
     imageData.exposure = exifRaw.ExposureTime;
     imageData.focalLength = exifRaw.FocalLength;
     imageData.aperture = exifRaw.FNumber;
